805d0437668a171b4adff7197166d8754d525a56
Dilip3399 <dilipkumardilip2000@gmail.com>
2026-06-29 03:09:35 +0530

    Fix process polyfill for Netlify build

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import process from "process/browser";
 
 import process from "process";
 import { Buffer } from "buffer";
