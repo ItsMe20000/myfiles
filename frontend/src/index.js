a424f26fe284a075fc6ca44d0ef08a36de1d8882
ItsMe <dilipkumardilip2000@gmail.com>
2026-06-29 03:24:49 +0530

    Remove redundant import of process module

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import process from "process/browser";
 
-import process from "process";
 import { Buffer } from "buffer";
 
 window.process = process;
